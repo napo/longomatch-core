51699702d2e8b50c18573cbbd34ccfa170d17573
Andoni Morales Alastruey <ylatuya@gmail.com>
2013-09-18 21:13:41 +0200

    Fix bitrate setting for mpeg4

--- libcesarplayer/video-utils.c
+++ libcesarplayer/video-utils.c
@@ -444,7 +444,7 @@ GstElement * lgm_create_video_encoder (VideoEncoderType type, guint quality,
       encoder = gst_element_factory_make ("ffenc_mpeg4", "video-encoder");
       g_object_set (encoder, "pass", 512,
           "max-key-interval", -1,
-          "bitrate", 4000000 / 100, NULL);
+          "bitrate", 4000000 * quality / 100, NULL);
       name = "FFmpeg mpeg4 video encoder";
       break;
 
