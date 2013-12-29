bb706fd926781db347d09017a3c46a4629d51340
Andoni Morales Alastruey <ylatuya@gmail.com>
2013-12-29 18:23:57 +0100

    Update video editor test utils

--- libcesarplayer/test-editor.c
+++ libcesarplayer/test-editor.c
@@ -58,22 +58,25 @@ main (int argc, char *argv[])
   gchar *input_file, *output_file;
   gchar *err = NULL;
   guint64 start, stop;
+  gint i;
 
   gst_video_editor_init_backend (&argc, &argv);
 
-  if (argc != 6) {
+  if (argc < 6) {
     g_print("Usage: test-remuxer input_file output_file format start stop\n");
     return 1;
   }
   input_file = argv[1];
   output_file = argv[2];
-  start = (guint64) g_strtod (argv[3], NULL);
-  stop = (guint64) g_strtod (argv[4], NULL);
 
   if (!g_strcmp0(argv[3], "mp4")) {
     video_encoder = VIDEO_ENCODER_H264;
     video_muxer = VIDEO_MUXER_MP4;
     audio_encoder = AUDIO_ENCODER_AAC;
+  } else if (!g_strcmp0(argv[3], "avi")) {
+    video_encoder = VIDEO_ENCODER_MPEG4;
+    video_muxer = VIDEO_MUXER_AVI;
+    audio_encoder = AUDIO_ENCODER_MP3;
   } else {
     err = g_strdup_printf ("Invalid format %s\n", argv[3]);
     goto error;
@@ -81,10 +84,18 @@ main (int argc, char *argv[])
 
   editor = gst_video_editor_new (NULL);
   gst_video_editor_set_encoding_format (editor, output_file, video_encoder,
-      audio_encoder, video_muxer, 500, 128, 240, 180, 25, 1, TRUE, TRUE);
+      audio_encoder, video_muxer, 500, 128, 1280, 720, 25, 1, TRUE, TRUE);
 
-  gst_video_editor_add_segment (editor, input_file, start, stop,
-      (gdouble) 1, "Test", TRUE);
+  for (i=5; i<=argc; i=i+2) {
+    gchar *title;
+    title = g_strdup_printf ("Title %d", i-4);
+
+    start = (guint64) g_strtod (argv[i-1], NULL);
+    stop = (guint64) g_strtod (argv[i], NULL);
+    gst_video_editor_add_segment (editor, input_file, start, stop-start,
+      (gfloat) 1, title, TRUE);
+    g_free (title);
+  }
 
   loop = g_main_loop_new (NULL, FALSE);
   g_signal_connect (editor, "error", G_CALLBACK (error_cb), editor);
