5e7daf4a8572ac13a5a4c5818593bc305bb017ff
Andoni Morales Alastruey <ylatuya@gmail.com>
2014-05-27 18:42:53 +0200

    Add + and - operators for Point

--- LongoMatch.Core/Store/Point.cs
+++ LongoMatch.Core/Store/Point.cs
@@ -79,5 +79,13 @@ namespace LongoMatch.Common
 		public static bool operator > (Point p1, Point p2) {
 			return p1.X > p2.X && p1.Y > p2.Y;
 		}
+		
+		public static Point operator + (Point p1, Point p2) {
+			return new Point (p1.X + p2.X, p1.Y + p2.Y);
+		}
+		
+		public static Point operator - (Point p1, Point p2) {
+			return new Point (p1.X - p2.X, p1.Y - p2.Y);
+		}
 	}
 }
