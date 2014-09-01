631072f7e96f3cff94953a853f8e2b7a82209415
Julien Moutte <julien@fluendo.com>
2014-09-01 11:40:00 +0200

    Embed datepicker and filechooser in an event box to ease theming and make the button widget belong to the composite widget.

--- LongoMatch.GUI/gtk-gui/LongoMatch.Gui.Component.DatePicker.cs
+++ LongoMatch.GUI/gtk-gui/LongoMatch.Gui.Component.DatePicker.cs
@@ -4,6 +4,7 @@ namespace LongoMatch.Gui.Component
 {
 	public partial class DatePicker
 	{
+		private global::Gtk.EventBox dateentryeventbox;
 		private global::Gtk.HBox hbox2;
 		private global::Gtk.Entry dateentry;
 		private global::Gtk.Button datebutton;
@@ -15,13 +16,18 @@ namespace LongoMatch.Gui.Component
 			global::Stetic.BinContainer.Attach (this);
 			this.Name = "LongoMatch.Gui.Component.DatePicker";
 			// Container child LongoMatch.Gui.Component.DatePicker.Gtk.Container+ContainerChild
+			this.dateentryeventbox = new global::Gtk.EventBox ();
+			this.dateentryeventbox.Name = "dateentryeventbox";
+			// Container child dateentryeventbox.Gtk.Container+ContainerChild
 			this.hbox2 = new global::Gtk.HBox ();
 			this.hbox2.Name = "hbox2";
+			this.hbox2.BorderWidth = ((uint)(2));
 			// Container child hbox2.Gtk.Box+BoxChild
 			this.dateentry = new global::Gtk.Entry ();
 			this.dateentry.CanFocus = true;
 			this.dateentry.Name = "dateentry";
 			this.dateentry.IsEditable = false;
+			this.dateentry.HasFrame = false;
 			this.dateentry.InvisibleChar = '•';
 			this.hbox2.Add (this.dateentry);
 			global::Gtk.Box.BoxChild w1 = ((global::Gtk.Box.BoxChild)(this.hbox2 [this.dateentry]));
@@ -50,7 +56,8 @@ namespace LongoMatch.Gui.Component
 			w10.Position = 1;
 			w10.Expand = false;
 			w10.Fill = false;
-			this.Add (this.hbox2);
+			this.dateentryeventbox.Add (this.hbox2);
+			this.Add (this.dateentryeventbox);
 			if ((this.Child != null)) {
 				this.Child.ShowAll ();
 			}
--- LongoMatch.GUI/gtk-gui/LongoMatch.Gui.Component.MediaFileChooser.cs
+++ LongoMatch.GUI/gtk-gui/LongoMatch.Gui.Component.MediaFileChooser.cs
@@ -4,6 +4,7 @@ namespace LongoMatch.Gui.Component
 {
 	public partial class MediaFileChooser
 	{
+		private global::Gtk.EventBox fileentryeventbox;
 		private global::Gtk.HBox hbox1;
 		private global::Gtk.Entry fileentry;
 		private global::Gtk.Button addbutton;
@@ -15,13 +16,18 @@ namespace LongoMatch.Gui.Component
 			global::Stetic.BinContainer.Attach (this);
 			this.Name = "LongoMatch.Gui.Component.MediaFileChooser";
 			// Container child LongoMatch.Gui.Component.MediaFileChooser.Gtk.Container+ContainerChild
+			this.fileentryeventbox = new global::Gtk.EventBox ();
+			this.fileentryeventbox.Name = "fileentryeventbox";
+			// Container child fileentryeventbox.Gtk.Container+ContainerChild
 			this.hbox1 = new global::Gtk.HBox ();
 			this.hbox1.Name = "hbox1";
+			this.hbox1.BorderWidth = ((uint)(2));
 			// Container child hbox1.Gtk.Box+BoxChild
 			this.fileentry = new global::Gtk.Entry ();
 			this.fileentry.CanFocus = true;
 			this.fileentry.Name = "fileentry";
 			this.fileentry.IsEditable = false;
+			this.fileentry.HasFrame = false;
 			this.fileentry.InvisibleChar = '•';
 			this.hbox1.Add (this.fileentry);
 			global::Gtk.Box.BoxChild w1 = ((global::Gtk.Box.BoxChild)(this.hbox1 [this.fileentry]));
@@ -50,7 +56,8 @@ namespace LongoMatch.Gui.Component
 			w10.Position = 1;
 			w10.Expand = false;
 			w10.Fill = false;
-			this.Add (this.hbox1);
+			this.fileentryeventbox.Add (this.hbox1);
+			this.Add (this.fileentryeventbox);
 			if ((this.Child != null)) {
 				this.Child.ShowAll ();
 			}
