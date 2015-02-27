e68effdd5c10bef0b0c3f04289c9be22a6bbb62b
Andoni Morales Alastruey <ylatuya@gmail.com>
2015-02-27 11:16:43 +0100

    Add a static switch to print output

--- Tests/Utils.cs
+++ Tests/Utils.cs
@@ -24,36 +24,46 @@ namespace Tests
 {
 	public class Utils
 	{
+		static bool debugLine = false;
+
 		public Utils ()
 		{
 		}
 		
-		public static T SerializeDeserialize<T> (T obj) {
+		public static T SerializeDeserialize<T> (T obj)
+		{
 			var stream = new MemoryStream ();
 			Serializer.Save (obj, stream, SerializationType.Json);
 			stream.Seek (0, SeekOrigin.Begin);
-			var jsonString = new StreamReader(stream).ReadToEnd();
-			Console.WriteLine (jsonString);
+			if (debugLine) {
+				var jsonString = new StreamReader(stream).ReadToEnd();
+				Console.WriteLine (jsonString);
+			}
 			stream.Seek(0, SeekOrigin.Begin);
 			
 			return Serializer.Load<T>(stream, SerializationType.Json);
 		}
 		 
-		public static void CheckSerialization<T> (T obj) {
+		public static void CheckSerialization<T> (T obj)
+		{
 			var stream = new MemoryStream ();
 			Serializer.Save (obj, stream, SerializationType.Json);
 			stream.Seek (0, SeekOrigin.Begin);
-			var jsonString = new StreamReader(stream).ReadToEnd();
-			Console.WriteLine (jsonString);
-			stream.Seek(0, SeekOrigin.Begin);
+			var jsonString = new StreamReader (stream).ReadToEnd ();
+			if (debugLine) {
+				Console.WriteLine (jsonString);
+			}
+			stream.Seek (0, SeekOrigin.Begin);
 			
-			var newobj = Serializer.Load<T>(stream, SerializationType.Json);
+			var newobj = Serializer.Load<T> (stream, SerializationType.Json);
 			
 			stream = new MemoryStream ();
 			Serializer.Save (newobj, stream, SerializationType.Json);
-			stream.Seek(0, SeekOrigin.Begin);
-			var newJsonString = new StreamReader(stream).ReadToEnd();
-			Console.WriteLine (newJsonString);
+			stream.Seek (0, SeekOrigin.Begin);
+			var newJsonString = new StreamReader (stream).ReadToEnd ();
+			if (debugLine) {
+				Console.WriteLine (newJsonString);
+			}
 			Assert.AreEqual (jsonString, newJsonString);
 		}
 	}
