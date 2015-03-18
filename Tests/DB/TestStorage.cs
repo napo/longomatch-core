779ce55c45744ed6a3b00ef43f1b8987388b8f88
Andoni Morales Alastruey <ylatuya@gmail.com>
2015-03-18 15:56:50 +0100

    Reuse the IDResolver as a cache for objects

--- Tests/DB/TestStorage.cs
+++ Tests/DB/TestStorage.cs
@@ -240,6 +240,38 @@ namespace Tests.DB
 		}
 
 		[Test ()]
+		[Ignore ("FIXME")]
+		public void TestSaveLoadTimelineEvent ()
+		{
+			IDReferenceResolver resolver = new IDReferenceResolver ();
+			Player p = new Player ();
+			AnalysisEventType evtType = new AnalysisEventType ();
+			TimelineEvent evt = new TimelineEvent ();
+
+			resolver.AddReference (null, p.ID.ToString (), p);
+			resolver.AddReference (null, evtType.ID.ToString (), evtType);
+			evt.Players.Add (p);
+			evt.EventType = evtType;
+
+			Document doc = db.GetDocument (evt.ID.ToString ());
+			doc.Update ((UnsavedRevision rev) => {
+				JObject jo = DocumentsSerializer.SerializeObject (evt, rev, db, resolver);
+				Assert.AreEqual (p.ID, jo ["Players"] [0].Value<Guid> ());
+				Assert.AreEqual (evtType.ID, jo ["EventType"].Value<Guid> ());
+				IDictionary<string, object> props = jo.ToObject<IDictionary<string, object>> ();
+				rev.SetProperties (props);
+				return true;
+			});
+
+			/* Player has not been added to the db, as it was already referenced
+			 * by the IDReferenceResolver */
+			Assert.AreEqual (1, db.DocumentCount);
+
+			TimelineEvent evt2 = storage.Retrieve <TimelineEvent> (evt.ID);
+			Assert.IsNotNull (evt2.EventType);
+		}
+
+		[Test ()]
 		public void TestSaveLoadProjectDescription ()
 		{
 			MediaFile mf = new MediaFile ("path", 34000, 25, true, true, "mp4", "h264",
@@ -293,7 +325,13 @@ namespace Tests.DB
 			p.Description = pd;
 
 			for (int i = 0; i < 10; i++) {
-				p.AddEvent (p.EventTypes [i], new Time (1000), new Time (2000), null, null, null, null);
+				TimelineEvent evt = new TimelineEvent {
+					EventType = p.EventTypes [i],
+					Start = new Time (1000),
+					Stop = new Time (2000),
+					Players = new List<Player> { p.LocalTeamTemplate.List [0] }, 
+				};
+				p.Timeline.Add (evt);
 			}
 
 			storage.Store<Project> (p);
@@ -303,6 +341,9 @@ namespace Tests.DB
 
 			Project p2 = storage.Retrieve<Project> (p.ID);
 			Assert.AreEqual (p.Timeline.Count, p2.Timeline.Count);
+			Assert.AreEqual (p2.LocalTeamTemplate.List [0], p2.Timeline [0].Players [0]);
+			//Assert.AreEqual ((p2.Dashboard.List [0] as AnalysisEventButton).EventType,
+			//	p2.Timeline [0].EventType);
 		}
 	}
 }
