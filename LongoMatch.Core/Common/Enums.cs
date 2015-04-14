ebb41e69b3b97ec68969a0b24db98397766a4761
Xavi Artigas <xartigas@fluendo.com>
2015-04-14 18:22:51 +0200

    Fixate some Enum values, as they must match the hard-coded values in the UI.

--- LongoMatch.Core/Common/Enums.cs
+++ LongoMatch.Core/Common/Enums.cs
@@ -428,17 +428,21 @@ namespace LongoMatch.Core.Common
 		Analysis,
 	}
 
+	/* The values must be kept in sync with the combobox in
+	 * LongoMatch.GUI/Gui/Component/LinkProperties.cs */
 	public enum LinkAction
 	{
-		Toggle,
-		Replicate,
+		Toggle = 0,
+		Replicate = 1,
 	}
 
+	/* The values must be kept in sync with the combobox in
+	 * LongoMatch.GUI/Gui/Component/LinkProperties.cs */
 	public enum TeamLinkAction
 	{
-		Clear,
-		Keep,
-		Invert
+		Clear = 0,
+		Keep = 1,
+		Invert = 2
 	}
 
 	public enum DashboardMode
