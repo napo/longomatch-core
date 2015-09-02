955fd1c3b8f19d4155a43770b16118a85abcab7e
Andoni Morales Alastruey <ylatuya@gmail.com>
2015-09-02 12:46:27 +0200

    Add tests for templates services

--- Tests/Services/TestTemplatesService.cs
+++ Tests/Services/TestTemplatesService.cs
@@ -15,25 +15,39 @@
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 //
-using NUnit.Framework;
 using System;
-using LongoMatch.Services;
-using LongoMatch.DB;
 using System.IO;
-using LongoMatch.Core.Store.Templates;
 using LongoMatch.Core.Common;
 using LongoMatch.Core.Interfaces;
+using LongoMatch.Core.Store.Templates;
+using LongoMatch.DB;
+using LongoMatch.Services;
+using NUnit.Framework;
 
 namespace Tests.Services
 {
 	[TestFixture ()]
 	public class TestTemplatesService
 	{
+		IStorage storage;
+		string tempPath;
+
+		[SetUp]
+		public void CreateStorage() {
+			tempPath = Path.Combine (Path.GetTempPath (), "TestTemplatesService");
+			try {
+				Directory.Delete (tempPath, true);
+			} catch {
+			}
+			storage = new FileStorage (tempPath);
+		}
+
 		[TearDown]
 		public void RemoveStorage ()
 		{
 			try {
-				Directory.Delete (Path.Combine (Path.GetTempPath (), "TestTemplatesService"), true);
+				storage.Reset ();
+				Directory.Delete (tempPath, true);
 			} catch {
 			}
 		}
@@ -41,28 +55,125 @@ namespace Tests.Services
 		[Test ()]
 		public void TestSystemTemplates ()
 		{
-			FileStorage fs = new FileStorage (
-				                 Path.Combine (Path.GetTempPath (), "TestTemplatesService"));
-			TemplatesService ts = new TemplatesService (fs);
+			TemplatesService ts = new TemplatesService (storage);
 			// Start service
 			ts.Start ();
 			ICategoriesTemplatesProvider ctp = ts.CategoriesTemplateProvider;
-
 			// We must have at least one template provider called 'Default'
 			Dashboard dash = ctp.Load ("Default dashboard");
-			Assert.AreNotSame (dash, null);
-
+			Assert.IsNotNull (dash);
 			// Test we dont have a template
-			bool found = ctp.Exists ("NonExistingTemplate");
-			Assert.AreEqual (found, false);
+			Assert.IsFalse (ctp.Exists ("NonExistingTemplate"));
 
-			// Test saving the default template
-			dash.Name = "NewDefault";
-			ctp.Save (dash);
+			ITeamTemplatesProvider ttp = ts.TeamTemplateProvider;
+			Assert.AreEqual (2, ttp.TemplatesNames.Count);
+			// Test we dont have a template
+			Assert.IsFalse (ctp.Exists ("NonExistingTemplate"));
+		}
 
-			// Test loading a template from a file
-			Dashboard newDefault = ctp.Load ("NewDefault");
-			Assert.AreEqual (newDefault.Name, "NewDefault");
+		[Test ()]
+		public void TestExists ()
+		{
+			CategoriesTemplatesProvider provider = new CategoriesTemplatesProvider (storage);
+			Assert.IsFalse (provider.Exists ("ACANDEMOR"));
+			Dashboard d = Dashboard.DefaultTemplate (10);
+			d.Name = "NEW";
+			provider.Save (d);
+			Assert.IsTrue (provider.Exists ("NEW"));
+		}
+
+		[Test()]
+		public void TestListTemplates()
+		{
+			CategoriesTemplatesProvider provider = new CategoriesTemplatesProvider (storage);
+			Assert.AreEqual (1, provider.Templates.Count);
+			Assert.AreEqual (1, provider.TemplatesNames.Count);
+			Dashboard d = Dashboard.DefaultTemplate (10);
+			d.Name = "NEW";
+			provider.Save (d);
+			Assert.AreEqual (2, provider.Templates.Count);
+			Assert.AreEqual (2, provider.TemplatesNames.Count);
+		}
+
+		[Test()]
+		public void TestSaveUpdateLoad ()
+		{
+			CategoriesTemplatesProvider provider = new CategoriesTemplatesProvider (storage);
+			Assert.Throws<TemplateNotFoundException<Dashboard>> (() => provider.Load ("jamematen"));
+			Dashboard d = Dashboard.DefaultTemplate (10);
+			d.Name = "jamematen";
+			provider.Save (d);
+			Assert.IsNotNull (provider.Load ("jamematen"));
+			provider.Update (d);
+			Assert.IsNotNull (provider.Load ("jamematen"));
+
+			d = Dashboard.DefaultTemplate (10);
+			d.Name = "system";
+			provider.Register (d);
+			Assert.IsNotNull (provider.Load ("system"));
+			// Check it returns a copy of the template
+			Assert.AreNotSame (d, provider.Load ("system"));
+		}
+
+		[Test()]
+		public void TestLoadFile ()
+		{
+			CategoriesTemplatesProvider provider = new CategoriesTemplatesProvider (storage);
+			Dashboard d = Dashboard.DefaultTemplate (10);
+			d.Name = "jamematen";
+			string path = Path.GetTempFileName ();
+			try {
+				Serializer.Save (d, path);
+				Assert.IsNotNull (provider.LoadFile (path));
+			} finally {
+				File.Delete (path);
+			}
+		}
+
+		[Test()]
+		public void TestRegister ()
+		{
+			CategoriesTemplatesProvider provider = new CategoriesTemplatesProvider (storage);
+			Dashboard d = Dashboard.DefaultTemplate (10);
+			d.Name = "system";
+			provider.Register (d);
+			Assert.IsNotNull (provider.Load ("system"));
+		}
+
+		[Test()]
+		public void TestCopy ()
+		{
+			CategoriesTemplatesProvider provider = new CategoriesTemplatesProvider (storage);
+			provider.Copy (provider.TemplatesNames[0], "NEW");
+			Assert.AreEqual (2, provider.TemplatesNames.Count);
+			Assert.IsNotNull (provider.Load ("NEW"));
+			Assert.Throws<TemplateNotFoundException<Dashboard>> (() => provider.Copy ("jamematen", "NEW"));
+		}
+
+		[Test()]
+		public void TestDelete ()
+		{
+			CategoriesTemplatesProvider provider = new CategoriesTemplatesProvider (storage);
+			Assert.AreEqual (1, provider.TemplatesNames.Count);
+			// Template does not exists
+			Assert.Throws<TemplateNotFoundException<Dashboard>> (() => provider.Delete ("jamematen"));
+			// System template
+			Assert.Throws<TemplateNotFoundException<Dashboard>> (() => provider.Delete (provider.TemplatesNames[0]));
+			Dashboard d = Dashboard.DefaultTemplate (10);
+			d.Name = "jamematen";
+			provider.Save (d);
+			Assert.AreEqual (2, provider.Templates.Count);
+			provider.Delete (d.Name);
+			Assert.AreEqual (1, provider.Templates.Count);
+		}
+
+		[Test()]
+		public void TestCreate ()
+		{
+			CategoriesTemplatesProvider provider = new CategoriesTemplatesProvider (storage);
+			provider.Create ("jamematen");
+			Assert.AreEqual (2, provider.TemplatesNames.Count);
+			Assert.IsNotNull (provider.Load ("jamematen"));
 		}
 	}
 }
