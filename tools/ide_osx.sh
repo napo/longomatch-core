ccea0a11d3b7635ab91b5b6262a01c0ba6368e18
Víctor Guzmán <vguzman@fluendo.com>
2015-09-29 12:54:44 +0200

    Fix typo in ide_osx
    Although by default osx is case-insensitive and this typo wouldn't
    fail, if someone has a case-sensitive filesystem it will.

--- tools/ide_osx.sh
+++ tools/ide_osx.sh
@@ -2,7 +2,7 @@
 export CERBERO_PREFIX=~/oneplay-build/dist/darwin_x86_64/
 export PATH=$PATH:~/oneplay-build/build-tools-darwin-x86_64/bin/:$CERBERO_PREFIX/bin/
 export PKG_CONFIG_PATH=$CERBERO_PREFIX/lib/pkgconfig/
-export MONO_PATH=$CERBERO_PREFIX/lib/mono/4.5/:$CERBERO_PREFIX/lib/mono/4.5/facades
+export MONO_PATH=$CERBERO_PREFIX/lib/mono/5.5/:$CERBERO_PREFIX/lib/mono/4.5/Facades
 export DYLD_FALLBACK_LIBRARY_PATH=$CERBERO_PREFIX/lib
 open /Applications/Xamarin\ Studio.app
 
