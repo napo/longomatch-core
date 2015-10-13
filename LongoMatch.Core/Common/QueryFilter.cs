21628e8640e046a13d1506c59b097c2b7ec806f9
Andoni Morales Alastruey <ylatuya@gmail.com>
2015-10-13 11:53:41 +0200

    Support passing lists and track changes in QueryFilter

--- LongoMatch.Core/Common/QueryFilter.cs
+++ LongoMatch.Core/Common/QueryFilter.cs
@@ -18,21 +18,75 @@
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace LongoMatch.Core.Common
 {
+	/// <summary>
+	/// A filter used to retrieve objects from the database using <see cref="IStorage.Retrieve</see>"/>.
+	/// </summary>
 	public class QueryFilter: Dictionary<string, List<object>>
 	{
+
+		Dictionary<string, List<object>> cachedFilter;
+
 		public QueryFilter ()
 		{
+			cachedFilter = new Dictionary<string, List<object>> ();
 			Operator = QueryOperator.And;
+
 		}
 
+		/// <summary>
+		/// Add a new filter constraint for an indexed property with a list of possible values.
+		/// </summary>
+		/// <param name="key">the name of the indexed property to filter .</param>
+		/// <param name="values">A list with the available options.</param>
 		public void Add (string key, params object[] values)
 		{
-			this [key] = values.ToList ();
+			List<object> valuesList, existingList = null;
+
+			if (values.Count () == 1 && values [0] is IEnumerable && !(values [0] is string)) {
+				valuesList = (values [0] as IEnumerable).OfType<object> ().ToList ();
+			} else {
+				valuesList = values.ToList ();
+			}
+			cachedFilter.TryGetValue (key, out existingList);
+			this [key] = valuesList;
 		}
 
+		/// <summary>
+		/// Saves the last changes to the filter.
+		/// </summary>
+		public void SaveChanges ()
+		{
+			cachedFilter.Clear ();
+			foreach (string key in Keys) {
+				cachedFilter [key] = this [key];
+			}
+		}
+
+		/// <summary>
+		/// Gets a value indicating whether this <see cref="LongoMatch.Core.Common.QueryFilter"/> has changed
+		/// with respect of the last call to <see cref="SaveChanges"/>
+		/// </summary>
+		public bool Changed {
+			get {
+				if (!cachedFilter.Keys.SequenceEqualNoOrder (Keys)) {
+					return true;
+				}
+				foreach (string key in cachedFilter.Keys) {
+					if (!cachedFilter [key].SequenceEqualNoOrder (this [key])) {
+						return true;
+					}
+				}
+				return false;
+			}
+		}
+
+		/// <summary>
+		/// Gets or sets the query operator type.
+		/// </summary>
 		public QueryOperator Operator {
 			get;
 			set;
