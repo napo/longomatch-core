4337579ff5f0a6d841988af5e8ddd9765e078dbf
Andoni Morales Alastruey <ylatuya@gmail.com>
2016-02-25 12:59:56 +0100

    Add new Clamp extension method

--- LongoMatch.Core/Common/ExtensionMethods.cs
+++ LongoMatch.Core/Common/ExtensionMethods.cs
@@ -97,5 +97,16 @@ namespace LongoMatch.Core.Common
 				coll.Add (item);
 			}
 		}
+
+		public static T Clamp<T> (this T val, T min, T max) where T : IComparable<T>
+		{
+			if (val.CompareTo (min) < 0) {
+				return min;
+			} else if (val.CompareTo (max) > 0) {
+				return max;
+			} else {
+				return val;
+			}
+		}
 	}
 }
