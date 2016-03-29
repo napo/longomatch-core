5a4e2c6c6819adc2f0be795bdc8bc7a98b211438
dfernandez <dfernandez@fluendo.com>
2016-03-29 11:24:22 +0200

    ProjectDescription.cs on LongoMatch.Core.Store becomes abstract
    Also create ProjectDescriptionLongomatch.cs that inherits from
    ProjectDescription.cs and migrate ProjectDescription.cs to VAS

--- LongoMatch.Core/Store/ProjectDescription.cs
+++ LongoMatch.Core/Store/ProjectDescription.cs
@@ -22,14 +22,14 @@ using LongoMatch.Core.Interfaces;
 using LongoMatch.Core.Common;
 using System.Runtime.Serialization;
 
-namespace LongoMatch.Core.Store
+namespace VAS.Core.Store
 {
 	/// <summary>
 	/// Describes a project in LongoMatch.
 	/// </summary>
 	[Serializable]
 	[PropertyChanged.ImplementPropertyChanged]
-	public class ProjectDescription : IChanged
+	abstract public class ProjectDescription : IChanged
 	{
 		DateTime matchDate, lastModified;
 
@@ -41,12 +41,8 @@ namespace LongoMatch.Core.Store
 			Competition = "";
 			Description = "";
 			Group = "";
-			LocalGoals = 0;
-			LocalName = "";
 			Phase = "";
 			Season = "";
-			VisitorGoals = 0;
-			VisitorName = "";
 		}
 
 		[JsonIgnore]
@@ -61,28 +57,14 @@ namespace LongoMatch.Core.Store
 		/// </summary>
 		[JsonIgnore]
 		[PropertyChanged.DoNotNotify]
-		public String Title {
-			get {
-				return String.Format ("{0} - {1} ({2}-{3}) {4} {5}",
-					LocalName, VisitorName, LocalGoals, VisitorGoals,
-					Competition, Season);
-			}
+		abstract public String Title {
+			get;
 		}
 
 		[JsonIgnore]
 		[PropertyChanged.DoNotNotify]
-		public String DateTitle {
-			get {
-				string ret = String.Format ("{0}-{1} {2}", LocalName, VisitorName,
-					             MatchDate.ToShortDateString ());
-				if (!String.IsNullOrEmpty (Season)) {
-					ret += " " + Season;
-				}
-				if (!String.IsNullOrEmpty (Competition)) {
-					ret += " " + Competition;
-				}
-				return ret;
-			}
+		abstract public String DateTitle {
+			get;
 		}
 
 		/// <summary>
@@ -138,56 +120,6 @@ namespace LongoMatch.Core.Store
 		}
 
 		/// <summary>
-		/// Name of the local team
-		/// </summary>
-		public String LocalName {
-			get;
-			set;
-		}
-
-		/// <summary>
-		/// Name of the visitor team
-		/// </summary>
-		public String VisitorName {
-			get;
-			set;
-		}
-
-		/// <summary>
-		/// Goals of the local team
-		/// </summary>
-		public int LocalGoals {
-			get;
-			set;
-		}
-
-		/// <summary>
-		/// Goals of the visitor team
-		/// </summary>
-		public int VisitorGoals {
-			get;
-			set;
-		}
-
-		/// <summary>
-		/// Gets or sets the shield for the local team.
-		/// </summary>
-		/// <value>The local team shield.</value>
-		public Image LocalShield {
-			get;
-			set;
-		}
-
-		/// <summary>
-		/// Gets or sets the shield for the visitor team.
-		/// </summary>
-		/// <value>The visitor team shield.</value>
-		public Image VisitorShield {
-			get;
-			set;
-		}
-
-		/// <summary>
 		/// Date of the game
 		/// </summary>
 		public DateTime MatchDate {
@@ -220,10 +152,6 @@ namespace LongoMatch.Core.Store
 			else if (Season != null && Season.IndexOf (text, sc) > -1)
 				return true;
 			else if (Competition != null && Competition.IndexOf (text, sc) > -1)
-				return true;
-			else if (LocalName != null && LocalName.IndexOf (text, sc) > -1)
-				return true;
-			else if (VisitorName != null && VisitorName.IndexOf (text, sc) > -1)
 				return true;
 			else if (Description != null && Description.IndexOf (text, sc) > -1)
 				return true;
--- LongoMatch.Core/Store/ProjectDescriptionLongoMatch.cs
+++ LongoMatch.Core/Store/ProjectDescriptionLongoMatch.cs
@@ -16,13 +16,112 @@
 //  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 //
 using System;
+using VAS.Core.Store;
+using Newtonsoft.Json;
 
 namespace LongoMatch.Core.Store
 {
-	public class ProjectDescriptionLongoMatch
+	[Serializable]
+	[PropertyChanged.ImplementPropertyChanged]
+	public class ProjectDescriptionLongoMatch : ProjectDescription
 	{
 		public ProjectDescriptionLongoMatch ()
 		{
+			LocalGoals = 0;
+			LocalName = "";
+			VisitorGoals = 0;
+			VisitorName = "";
+		}
+
+		[JsonIgnore]
+		[PropertyChanged.DoNotNotify]
+		public override String Title {
+			get {
+				return String.Format ("{0} - {1} ({2}-{3}) {4} {5}",
+					LocalName, VisitorName, LocalGoals, VisitorGoals,
+					Competition, Season);
+			}
+		}
+
+		[JsonIgnore]
+		[PropertyChanged.DoNotNotify]
+		public override String DateTitle {
+			get {
+				string ret = String.Format ("{0}-{1} {2}", LocalName, VisitorName,
+					             MatchDate.ToShortDateString ());
+				if (!String.IsNullOrEmpty (Season)) {
+					ret += " " + Season;
+				}
+				if (!String.IsNullOrEmpty (Competition)) {
+					ret += " " + Competition;
+				}
+				return ret;
+			}
+		}
+
+		/// <summary>
+		/// Name of the local team
+		/// </summary>
+		public String LocalName {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Name of the visitor team
+		/// </summary>
+		public String VisitorName {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Goals of the local team
+		/// </summary>
+		public int LocalGoals {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Goals of the visitor team
+		/// </summary>
+		public int VisitorGoals {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the shield for the local team.
+		/// </summary>
+		/// <value>The local team shield.</value>
+		public Image LocalShield {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the shield for the visitor team.
+		/// </summary>
+		/// <value>The visitor team shield.</value>
+		public Image VisitorShield {
+			get;
+			set;
+		}
+
+		public bool Search (string text)
+		{
+			StringComparison sc = StringComparison.InvariantCultureIgnoreCase;
+
+			if (!base.Search (text)) {
+				if (LocalName != null && LocalName.IndexOf (text, sc) > -1) {
+					return true;
+				} else if (VisitorName != null && VisitorName.IndexOf (text, sc) > -1) {
+					return true;
+				} else {
+					return false;
+				}
+			}
 		}
 	}
 }
