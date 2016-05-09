350e7cf7258873a3926ef328b934deabbcbb52b2
Andoni Morales Alastruey <ylatuya@gmail.com>
2016-05-09 18:45:27 +0200

    Remove LongoMatch from filenames

--- LongoMatch.Core/Common/Exceptions.cs
+++ LongoMatch.Core/Common/Exceptions.cs
@@ -1,4 +1,4 @@
-//
+﻿//
 //  Copyright (C) 2013 Andoni Morales Alastruey
 //
 //  This program is free software; you can redistribute it and/or modify
@@ -16,59 +16,16 @@
 //  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
 //
 using System;
-using System.Collections.Generic;
-using LongoMatch.Core.Store;
 using LongoMatch.Core.Store.Templates;
-using LongoMatch.Core.Interfaces;
+using VAS.Core;
+using VAS.Core.Interfaces;
+using VAS.Core.Store.Templates;
 
 namespace LongoMatch.Core.Common
 {
-	public class DBLockedException: Exception
-	{
-		public DBLockedException (Exception innerException) :
-			base (Catalog.GetString ("Database locked:" + innerException.Message),
-			      innerException)
-		{
-		}
-	}
-
-	public class UnknownDBErrorException: Exception
-	{
-		public UnknownDBErrorException (Exception innerException) :
-			base (Catalog.GetString ("Unknown database error:" + innerException),
-			      innerException)
-		{
-		}
-	}
-
 	public class SubstitutionException: Exception
 	{
 		public SubstitutionException (string error) : base (error)
-		{
-		}
-	}
-
-	public class ProjectDeserializationException: Exception
-	{
-		public ProjectDeserializationException (Exception innerException) :
-			base (Catalog.GetString ("Project loading failed:") + innerException,
-			      innerException)
-		{
-		}
-	}
-
-	public class ProjectNotFoundException: Exception
-	{
-		public ProjectNotFoundException (string file) :
-			base (Catalog.GetString ("Project file not found:\n") + file)
-		{
-		}
-	}
-
-	public class InvalidTemplateFilenameException: Exception
-	{
-		public InvalidTemplateFilenameException (List<char> invalidChars) :
-			base (Catalog.GetString ("The name contains invalid characters: ") + String.Join (" ", invalidChars))
 		{
 		}
 	}
@@ -91,65 +48,4 @@ namespace LongoMatch.Core.Common
 			}
 		}
 	}
-
-	public class HotkeyAlreadyInUse: Exception
-	{
-		public HotkeyAlreadyInUse (HotKey hotkey) :
-			base (Catalog.GetString ("Hotkey already in use: ") + hotkey)
-		{
-		}
-	}
-
-	public class TimerNotRunningException: Exception
-	{
-	}
-
-	/// <summary>
-	/// An exception thrown by an addin to request a shutdown of the application
-	/// </summary>
-	public class AddinRequestShutdownException: Exception
-	{
-		public AddinRequestShutdownException (string reason) :
-			base (reason)
-		{
-		}
-	}
-
-	/// <summary>
-	/// An exception thrown by an addin in its initialization if it can't be used for some reason
-	/// </summary>
-	public class AddinUnusableException: Exception
-	{
-		public AddinUnusableException (string reason) :
-			base (reason)
-		{
-		}
-	}
-
-	public class CircularDependencyException: Exception
-	{
-	}
-
-	public class ImportException: Exception
-	{
-		public ImportException (string reason) : base (reason)
-		{
-		}
-	}
-
-	public class InvalidQueryException: Exception
-	{
-		public InvalidQueryException (string reason) : base (reason)
-		{
-		}
-	}
-
-	public class StorageException: Exception
-	{
-		public StorageException (string reason) : base (reason)
-		{
-		}
-	}
-
 }
-
