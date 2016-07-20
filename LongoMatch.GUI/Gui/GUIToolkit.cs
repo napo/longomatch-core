0e201f0844b5b0d44d85e51c952b861e0dd1ca4c
aameijide <aameijide@fluendo.com>
2016-07-20 15:40:28 +0200

    Adapt view to IPanel

--- LongoMatch.GUI/Gui/GUIToolkit.cs
+++ LongoMatch.GUI/Gui/GUIToolkit.cs
@@ -344,7 +344,7 @@ namespace LongoMatch.Gui
 			controller.SetViewModel (dashboardsVM);
 			controller.Start ();
 			Log.Information ("Open sports templates manager");
-			mainWindow.SetPanel ((Widget)view);
+			mainWindow.SetPanel ((IPanel)view);
 			(view as Bin).DeleteEvent += (o, args) => controller.Stop ();
 		}
 
