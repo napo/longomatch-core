e8fdc2ef3951c8f863f3f617f4f96412a7cde99b
aameijide <aameijide@fluendo.com>
2016-09-06 10:41:50 +0200

    Update Xamarin scripts

--- tools/ide_linux.sh
+++ tools/ide_linux.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-export CERBERO_PREFIX=~/oneplay-build/dist/linux_x86_64/
-export PATH=$PATH:~/oneplay-build/build-tools-linux-x86_64/bin/
+export CERBERO_PREFIX=/opt/longomatch-build_linux_linux_x86_64/dist/linux_x86_64/
+export PATH=$PATH:/opt/longomatch-build_linux_linux_x86_64/build-tools/bin/
 export PKG_CONFIG_PATH=$CERBERO_PREFIX/lib/pkgconfig/
 export MONO_PATH=$CERBERO_PREFIX/lib/mono/4.5/:$CERBERO_PREFIX/lib/mono/4.5/Facades
 export LD_LIBRARY_PATH=$CERBERO_PREFIX/lib
-monodevelop $@
+monodevelop $@ &
 
