83e8971ec6d1050b4dca4c6bbaeb5c3017675594
Víctor Guzmán <vguzman@fluendo.com>
2016-09-21 16:55:12 +0200

    Add  SQLitePCLPlugin_esqlite3

--- build/build_env.mk
+++ build/build_env.mk
@@ -1,6 +1,7 @@
 LM_DEPS = \
 	$(top_builddir)/bin/Couchbase.Lite.dll \
 	$(top_builddir)/bin/Couchbase.Lite.Storage.SystemSQLite.dll \
+	$(top_builddir)/bin/SQLitePCLPlugin_esqlite3.dll \
 	$(top_builddir)/bin/Newtonsoft.Json.dll \
 	$(top_builddir)/bin/Stateless.dll \
 	$(top_builddir)/bin/SQLitePCL.raw.dll \
