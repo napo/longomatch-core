9f3722ccb048e72cc381a343ac9f47cc35426f72
aoller <aoller@fluendo.com>
2016-10-17 17:14:37 +0200

    Refactor Registry and implement RegistryAttribute

---
+++ LongoMatch.Core/Interfaces/Services/IDeviceServices.cs
@@ -0,0 +1,32 @@
+﻿//
+//  Copyright (C) 2016 Fluendo S.A.
+//
+//
+using System.Threading.Tasks;
+
+namespace LongoMatch.Core.Interfaces.Services
+{
+	/// <summary>
+	/// Interface for exposing native device services
+	/// </summary>
+	public interface IDeviceServices
+	{
+		void ShareFiles (string [] filePaths, bool emailEnabled);
+
+		/// <summary>
+		/// Return if camera and micro recording is allowed. 
+		/// If permissions are denied by default, tries to request for permissions.
+		/// </summary>
+		/// <returns>The capture permission allowed.</returns>
+		Task<bool> CheckCapturePermissions ();
+
+		/// <summary>
+		/// Return if external storage access is allowed. 
+		/// If permissions are denied by default, tries to request for permissions.
+		/// If the device doesn't support external storage, alwais returns false.
+		/// </summary>
+		/// <returns>The external storage permission allowed.</returns>
+		Task<bool> CheckExternalStoragePermission ();
+	}
+}
+
---
+++ LongoMatch.Core/Services/RegistryAttribute.cs
@@ -0,0 +1,32 @@
+﻿//
+//  Copyright (C) 2016 Fluendo S.A.
+//
+//
+using System;
+namespace LongoMatch.Core.Services
+{
+	/// <summary>
+	/// Attribute used to register IView components.
+	/// </summary>
+	[AttributeUsage (AttributeTargets.Class, AllowMultiple = true)]
+	public class RegistryAttribute : Attribute
+	{
+		public RegistryAttribute (Type interfaceType, int priority)
+		{
+			InterfaceType = interfaceType;
+			Priority = priority;
+		}
+
+		/// <summary>
+		/// Gets or sets the type of the interface implemented.
+		/// </summary>
+		/// <value>The type of the interface implemented.</value>
+		public Type InterfaceType { get; set; }
+
+		/// <summary>
+		/// Gets or sets the priority.
+		/// </summary>
+		/// <value>The priority.</value>
+		public int Priority { get; set; }
+	}
+}
