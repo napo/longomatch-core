1e4b2e2db315d04a945d6938e42ab4ee9cc8942b
Sandra Pallarés <sandra@Sandras-MacBook-Pro.local>
2017-05-19 10:23:14 +0200

    simplify makefiles + mv files from core to longomatch

--- tools/ide_linux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export CERBERO_PREFIX=/opt/longomatch-build_linux_linux_x86_64/dist/linux_x86_64/
-export PATH=$PATH:/opt/longomatch-build_linux_linux_x86_64/build-tools/bin/
-export PKG_CONFIG_PATH=$CERBERO_PREFIX/lib/pkgconfig/
-export MONO_PATH=$CERBERO_PREFIX/lib/mono/4.5/:$CERBERO_PREFIX/lib/mono/4.5/Facades
-export LD_LIBRARY_PATH=$CERBERO_PREFIX/lib
-monodevelop $@ &
-
--- tools/ide_osx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-export CERBERO_PREFIX=/opt/longomatch-build_darwin_darwin_x86_64/dist/darwin_x86_64/
-export PATH=$PATH:/opt/longomatch-build_darwin_darwin_x86_64/build-tools-darwin-x86_64/bin/:$CERBERO_PREFIX/bin/
-export PKG_CONFIG_PATH=$CERBERO_PREFIX/lib/pkgconfig/
-export MONO_PATH=$CERBERO_PREFIX/lib/mono/4.5/:$CERBERO_PREFIX/lib/mono/4.5/Facades
-export DYLD_FALLBACK_LIBRARY_PATH=$CERBERO_PREFIX/lib
-open /Applications/Xamarin\ Studio.app $@ -n
-
