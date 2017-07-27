54cf47499dec712c5a369080f2c5db5bf2b0bad1
Victor Martos <vmartos@fluendo.com>
2017-07-27 17:43:57 +0200

    Add Tests for LMLicenseLimitationService

---
+++ Tests/Services/TestLMLicenseLimitationsService.cs
@@ -0,0 +1,138 @@
+﻿//
+//  Copyright (C) 2017 Fluendo S.A.
+using System;
+using System.Threading.Tasks;
+using LongoMatch;
+using LongoMatch.Core.Common;
+using LongoMatch.License;
+using LongoMatch.Services;
+using Moq;
+using NUnit.Framework;
+using VAS.Core.Events;
+using VAS.Core.Interfaces.License;
+using VAS.Core.ViewModel;
+
+namespace Tests.Services
+{
+	[TestFixture]
+	public class TestLMLicenseLimitationsService
+	{
+		LMLicenseLimitationsService service;
+		ILicenseManager currentLicenseManager;
+		LMDummyWibuManager wibuManager;
+
+		[TestFixtureSetUp]
+		public void TestFixtureSetUp ()
+		{
+			currentLicenseManager = App.Current.LicenseManager;
+		}
+
+		[TestFixtureTearDown]
+		public void TestFixtureTearDown ()
+		{
+			App.Current.LicenseManager = currentLicenseManager;
+		}
+
+		[Test]
+		public async Task LMLicenseLimitationsService_ProPanInitialized_DatabaseManagerLimitationDisabled ()
+		{
+			wibuManager = new LMDummyWibuManager (LMDummyWibuManager.PRO_PRODUCT_TEXT);
+			App.Current.LicenseManager = wibuManager;
+			await App.Current.LicenseManager.Init ();
+			service = new LMLicenseLimitationsService ();
+			var featureLimitation = service.Get<FeatureLimitationVM> (Constants.DATABASE_MANAGER_FEATURE);
+
+			Assert.IsNotNull (featureLimitation);
+			Assert.AreEqual (Constants.DATABASE_MANAGER_FEATURE, featureLimitation.LimitationName);
+			Assert.IsFalse (featureLimitation.Enabled);
+		}
+
+		[Test]
+		public async Task LMLicenseLimitationsService_StarterPanInitialized_DatabaseManagerLimitationDisabled ()
+		{
+			wibuManager = new LMDummyWibuManager (LMDummyWibuManager.STARTER_PRODUCT_TEXT);
+			App.Current.LicenseManager = wibuManager;
+			await App.Current.LicenseManager.Init ();
+			service = new LMLicenseLimitationsService ();
+			var featureLimitation = service.Get<FeatureLimitationVM> (Constants.DATABASE_MANAGER_FEATURE);
+
+			Assert.IsNotNull (featureLimitation);
+			Assert.AreEqual (Constants.DATABASE_MANAGER_FEATURE, featureLimitation.LimitationName);
+			Assert.IsFalse (featureLimitation.Enabled);
+		}
+
+		[Test]
+		public async Task LMLicenseLimitationsService_BasicPlanInitialized_DatabaseManagerLimitationEnabled ()
+		{
+			wibuManager = new LMDummyWibuManager (LMDummyWibuManager.BASIC_PRODUCT_TEXT);
+			App.Current.LicenseManager = wibuManager;
+			await App.Current.LicenseManager.Init ();
+			service = new LMLicenseLimitationsService ();
+			var featureLimitation = service.Get<FeatureLimitationVM> (Constants.DATABASE_MANAGER_FEATURE);
+
+			Assert.IsNotNull (featureLimitation);
+			Assert.AreEqual (Constants.DATABASE_MANAGER_FEATURE, featureLimitation.LimitationName);
+			Assert.IsTrue (featureLimitation.Enabled);
+		}
+
+		[Test]
+		public async Task LMLicenseLimitationsService_LicenseChangeEventPro_DatabaseManagerLimitationDisabled ()
+		{
+			wibuManager = new LMDummyWibuManager (LMDummyWibuManager.BASIC_PRODUCT_TEXT);
+			App.Current.LicenseManager = wibuManager;
+			await App.Current.LicenseManager.Init ();
+			service = new LMLicenseLimitationsService ();
+			service.Start ();
+			var featureLimitation = service.Get<FeatureLimitationVM> (Constants.DATABASE_MANAGER_FEATURE);
+			Assert.IsTrue (featureLimitation.Enabled);
+
+			wibuManager = new LMDummyWibuManager (LMDummyWibuManager.PRO_PRODUCT_TEXT);
+			App.Current.LicenseManager = wibuManager;
+			await App.Current.LicenseManager.Init ();
+			await App.Current.EventsBroker.Publish (new LicenseChangeEvent ());
+
+			Assert.IsFalse (featureLimitation.Enabled);
+			service.Stop ();
+		}
+
+		[Test]
+		public async Task LMLicenseLimitationsService_LicenseChangeEventStarter_DatabaseManagerLimitationDisabled ()
+		{
+			wibuManager = new LMDummyWibuManager (LMDummyWibuManager.BASIC_PRODUCT_TEXT);
+			App.Current.LicenseManager = wibuManager;
+			await App.Current.LicenseManager.Init ();
+			service = new LMLicenseLimitationsService ();
+			service.Start ();
+			var featureLimitation = service.Get<FeatureLimitationVM> (Constants.DATABASE_MANAGER_FEATURE);
+			Assert.IsTrue (featureLimitation.Enabled);
+
+			wibuManager = new LMDummyWibuManager (LMDummyWibuManager.STARTER_PRODUCT_TEXT);
+			App.Current.LicenseManager = wibuManager;
+			await App.Current.LicenseManager.Init ();
+			await App.Current.EventsBroker.Publish (new LicenseChangeEvent ());
+
+			Assert.IsFalse (featureLimitation.Enabled);
+			service.Stop ();
+		}
+
+		[Test]
+		public async Task LMLicenseLimitationsService_LicenseChangeEventBasic_DatabaseManagerLimitationDisabled ()
+		{
+			wibuManager = new LMDummyWibuManager (LMDummyWibuManager.PRO_PRODUCT_TEXT);
+			App.Current.LicenseManager = wibuManager;
+			await App.Current.LicenseManager.Init ();
+			service = new LMLicenseLimitationsService ();
+			service.Start ();
+			var featureLimitation = service.Get<FeatureLimitationVM> (Constants.DATABASE_MANAGER_FEATURE);
+			Assert.IsFalse (featureLimitation.Enabled);
+
+			wibuManager = new LMDummyWibuManager (LMDummyWibuManager.BASIC_PRODUCT_TEXT);
+			App.Current.LicenseManager = wibuManager;
+			await App.Current.LicenseManager.Init ();
+			await App.Current.EventsBroker.Publish (new LicenseChangeEvent ());
+
+			Assert.IsTrue (featureLimitation.Enabled);
+			service.Stop ();
+		}
+	}
+}
