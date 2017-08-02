09e43221c63cb50b64828d45e7532fcdf692c950
Sandra Pallarés <spallares@fluendo.com>
2017-08-02 16:38:13 +0200

    Add limitation for excel exporter

---
+++ Tests/Plugins/Exporters/TestCsvExporter.cs
@@ -0,0 +1,71 @@
+﻿//
+//  Copyright (C) 2017 FLUENDO S.A.
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
+//
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using LongoMatch;
+using LongoMatch.Core.Common;
+using LongoMatch.Core.Store;
+using LongoMatch.Plugins;
+using Moq;
+using NUnit.Framework;
+using VAS.Core.Interfaces;
+using VAS.Core.Store;
+
+namespace Tests.Plugins.Exporters
+{
+	public class TestLongoMatchExporter
+	{
+		[Test]
+		public async Task ExportProject_NoLimitation_ExportFinishedOk ()
+		{
+			// Arrange
+			LMProject p = null;
+			string tmp = Path.GetTempFileName ();
+			if (File.Exists (tmp)) {
+				File.Delete (tmp);
+			}
+
+			try {
+				p = Utils.CreateProject (false);
+				DummyLongoMAtchExporterExporter exporter = new DummyLongoMAtchExporterExporter ();
+
+				// Act
+				await exporter.Export (p, tmp);
+
+				// Assert
+				Assert.IsTrue (File.Exists (tmp));
+				Assert.IsTrue (exporter.ExportDone);
+
+			} finally {
+				Utils.DeleteProject (p);
+				if (File.Exists (tmp)) {
+					File.Delete (tmp);
+				}
+			}
+		}
+	}
+
+	class DummyLongoMAtchExporterExporter : LongoMatchExporter
+	{
+		public async Task Export (Project project, string filename)
+		{
+			await this.ExportProject (project, filename);
+		}
+	}
+}
--- Tests/Services/TestLMLicenseLimitationsService.cs
+++ Tests/Services/TestLMLicenseLimitationsService.cs
@@ -85,6 +85,20 @@ namespace Tests.Services
 		}
 
 		[Test]
+		public async Task LMLicenseLimitationsService_BasicPlanInitialized_ExcelExportLimitationEnabled ()
+		{
+			wibuManager = new LMDummyWibuManager (LMDummyWibuManager.BASIC_PRODUCT_TEXT);
+			App.Current.LicenseManager = wibuManager;
+			await App.Current.LicenseManager.Init ();
+			service = new LMLicenseLimitationsService ();
+			var featureLimitation = service.Get<FeatureLimitationVM> (LongoMatchFeature.ExcelExport.ToString ());
+
+			Assert.IsNotNull (featureLimitation);
+			Assert.AreEqual (LongoMatchFeature.ExcelExport.ToString (), featureLimitation.RegisterName);
+			Assert.IsTrue (featureLimitation.Enabled);
+		}
+
+		[Test]
 		public async Task LMLicenseLimitationsService_LicenseChangeEventPro_DatabaseManagerLimitationDisabled ()
 		{
 			wibuManager = new LMDummyWibuManager (LMDummyWibuManager.BASIC_PRODUCT_TEXT);
@@ -113,6 +127,26 @@ namespace Tests.Services
 			service = new LMLicenseLimitationsService ();
 			service.Start ();
 			var featureLimitation = service.Get<FeatureLimitationVM> (LongoMatchFeature.VideoConverter.ToString ());
+			Assert.IsTrue (featureLimitation.Enabled);
+
+			wibuManager = new LMDummyWibuManager (LMDummyWibuManager.PRO_PRODUCT_TEXT);
+			App.Current.LicenseManager = wibuManager;
+			await App.Current.LicenseManager.Init ();
+			await App.Current.EventsBroker.Publish (new LicenseChangeEvent ());
+
+			Assert.IsFalse (featureLimitation.Enabled);
+			service.Stop ();
+		}
+
+		[Test]
+		public async Task LMLicenseLimitationsService_LicenseChangeEventPro_ExcelExportLimitationDisabled ()
+		{
+			wibuManager = new LMDummyWibuManager (LMDummyWibuManager.BASIC_PRODUCT_TEXT);
+			App.Current.LicenseManager = wibuManager;
+			await App.Current.LicenseManager.Init ();
+			service = new LMLicenseLimitationsService ();
+			service.Start ();
+			var featureLimitation = service.Get<FeatureLimitationVM> (LongoMatchFeature.ExcelExport.ToString ());
 			Assert.IsTrue (featureLimitation.Enabled);
 
 			wibuManager = new LMDummyWibuManager (LMDummyWibuManager.PRO_PRODUCT_TEXT);
@@ -165,7 +199,27 @@ namespace Tests.Services
 		}
 
 		[Test]
-		public async Task LMLicenseLimitationsService_LicenseChangeEventBasic_DatabaseManagerLimitationDisabled ()
+		public async Task LMLicenseLimitationsService_LicenseChangeEventStarter_ExportExcelLimitationEnabled ()
+		{
+			wibuManager = new LMDummyWibuManager (LMDummyWibuManager.PRO_PRODUCT_TEXT);
+			App.Current.LicenseManager = wibuManager;
+			await App.Current.LicenseManager.Init ();
+			service = new LMLicenseLimitationsService ();
+			service.Start ();
+			var featureLimitation = service.Get<FeatureLimitationVM> (LongoMatchFeature.ExcelExport.ToString ());
+			Assert.IsFalse (featureLimitation.Enabled);
+
+			wibuManager = new LMDummyWibuManager (LMDummyWibuManager.STARTER_PRODUCT_TEXT);
+			App.Current.LicenseManager = wibuManager;
+			await App.Current.LicenseManager.Init ();
+			await App.Current.EventsBroker.Publish (new LicenseChangeEvent ());
+
+			Assert.IsTrue (featureLimitation.Enabled);
+			service.Stop ();
+		}
+
+		[Test]
+		public async Task LMLicenseLimitationsService_LicenseChangeEventBasic_DatabaseManagerLimitationEnabled ()
 		{
 			wibuManager = new LMDummyWibuManager (LMDummyWibuManager.PRO_PRODUCT_TEXT);
 			App.Current.LicenseManager = wibuManager;
@@ -185,7 +239,7 @@ namespace Tests.Services
 		}
 
 		[Test]
-		public async Task LMLicenseLimitationsService_LicenseChangeEventBasic_ConversionLimitationDisabled ()
+		public async Task LMLicenseLimitationsService_LicenseChangeEventBasic_ConversionLimitationEnabled ()
 		{
 			wibuManager = new LMDummyWibuManager (LMDummyWibuManager.PRO_PRODUCT_TEXT);
 			App.Current.LicenseManager = wibuManager;
