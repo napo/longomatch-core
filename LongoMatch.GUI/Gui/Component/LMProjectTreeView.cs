d275bae425086549168b538a93a8c113ee9d9884
dfernandez <dfernandez@fluendo.com>
2017-09-27 11:35:54 +0200

    fixup! Unselect TreeViewBase selection before each selection change

--- LongoMatch.GUI/Gui/Component/LMProjectTreeView.cs
+++ LongoMatch.GUI/Gui/Component/LMProjectTreeView.cs
@@ -113,13 +113,5 @@ namespace LongoMatch.Gui.Component
 				base.RemoveSubViewModel (subViewModel);
 			}
 		}
-
-		protected override void HandleViewModelPropertyChanged (object sender, System.ComponentModel.PropertyChangedEventArgs e)
-		{
-			if (e.PropertyName == "Collection_" + nameof (ViewModel.Selection) && Selection.CountSelectedRows () > 0) {
-				Selection.UnselectAll ();
-			}
-			base.HandleViewModelPropertyChanged (sender, e);
-		}
 	}
 }
