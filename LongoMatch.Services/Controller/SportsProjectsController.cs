50c49b23186bf847685966ea1b6e548623056561
Dani Garcia Sanchez <dgarcia@fluendo.com>
2017-11-30 18:04:34 +0100

    Fix indentation problems and remove fake projects

--- LongoMatch.Services/Controller/SportsProjectsController.cs
+++ LongoMatch.Services/Controller/SportsProjectsController.cs
@@ -24,85 +24,73 @@ namespace LongoMatch.Services.Controller
 	/// <summary>
 	/// Controller for sports projects.
 	/// </summary>
-	[ControllerAttribute(ProjectsManagerState.NAME)]
+	[ControllerAttribute (ProjectsManagerState.NAME)]
 	public class SportsProjectsController : ProjectsController<LMProject, LMProjectVM>
 	{
 
-		public new SportsProjectsManagerVM ViewModel
-		{
-			get
-			{
+		public new SportsProjectsManagerVM ViewModel {
+			get {
 				return (SportsProjectsManagerVM)base.ViewModel;
 			}
-			set
-			{
+			set {
 				base.ViewModel = value;
 			}
 		}
 
-		public override async Task Start()
+		public override async Task Start ()
 		{
-			await base.Start();
-			App.Current.EventsBroker.SubscribeAsync<ResyncEvent>(HandleResync);
-			App.Current.EventsBroker.Subscribe<OpenEvent<LMProject>>(HandleOpen);
-			App.Current.EventsBroker.Subscribe<SearchEvent<LMProject>>(HandleSearchEvent);
+			await base.Start ();
+			App.Current.EventsBroker.SubscribeAsync<ResyncEvent> (HandleResync);
+			App.Current.EventsBroker.Subscribe<OpenEvent<LMProject>> (HandleOpen);
+			App.Current.EventsBroker.Subscribe<SearchEvent<LMProject>> (HandleSearchEvent);
 		}
 
-		public override async Task Stop()
+		public override async Task Stop ()
 		{
-			await base.Stop();
-			App.Current.EventsBroker.UnsubscribeAsync<ResyncEvent>(HandleResync);
-			App.Current.EventsBroker.Unsubscribe<OpenEvent<LMProject>>(HandleOpen);
-			App.Current.EventsBroker.Unsubscribe<SearchEvent<LMProject>>(HandleSearchEvent);
+			await base.Stop ();
+			App.Current.EventsBroker.UnsubscribeAsync<ResyncEvent> (HandleResync);
+			App.Current.EventsBroker.Unsubscribe<OpenEvent<LMProject>> (HandleOpen);
+			App.Current.EventsBroker.Unsubscribe<SearchEvent<LMProject>> (HandleSearchEvent);
 		}
 
-		protected override void HandleSelectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+		protected override void HandleSelectionChanged (object sender, NotifyCollectionChangedEventArgs e)
 		{
-			base.HandleSelectionChanged(sender, e);
-			ViewModel.ResyncCommand.EmitCanExecuteChanged();
+			base.HandleSelectionChanged (sender, e);
+			ViewModel.ResyncCommand.EmitCanExecuteChanged ();
 		}
 
-		async Task HandleResync(ResyncEvent ev)
+		async Task HandleResync (ResyncEvent ev)
 		{
-			if (!ViewModel.LoadedProject.FileSet.Model.CheckFiles())
-			{
+			if (!ViewModel.LoadedProject.FileSet.Model.CheckFiles ()) {
 				// Show message in order to load video.
-				if (!App.Current.GUIToolkit.SelectMediaFiles(ViewModel.LoadedProject.FileSet.Model))
-				{
+				if (!App.Current.GUIToolkit.SelectMediaFiles (ViewModel.LoadedProject.FileSet.Model)) {
 					return;
 				}
 			}
 
-			dynamic data = new System.Dynamic.ExpandoObject();
+			dynamic data = new System.Dynamic.ExpandoObject ();
 
-			ViewModel.LoadedProject.Model.Load();
+			ViewModel.LoadedProject.Model.Load ();
 			data.ProjectVM = ViewModel.LoadedProject;
 			data.SynchronizeEventsWithPeriods = false;
-			await App.Current.StateController.MoveTo(CameraSynchronizationEditorState.NAME, data);
-			ViewModel.SaveCommand.EmitCanExecuteChanged();
+			await App.Current.StateController.MoveTo (CameraSynchronizationEditorState.NAME, data);
+			ViewModel.SaveCommand.EmitCanExecuteChanged ();
 		}
 
-		void HandleOpen(OpenEvent<LMProject> arg)
+		void HandleOpen (OpenEvent<LMProject> arg)
 		{
-			if (ViewModel.LoadedProject != null)
-			{
+			if (ViewModel.LoadedProject != null) {
 				// We get the selection instead of LoadedProject because it can be modified without saving.
 				// Also we don't use the selected VM directly because it's disposed on unload
-				LMProjectVM selectedVM = new LMProjectVM { Model = ViewModel.Selection.First().Model };
-				LMStateHelper.OpenProject(selectedVM);
+				LMProjectVM selectedVM = new LMProjectVM { Model = ViewModel.Selection.First ().Model };
+				LMStateHelper.OpenProject (selectedVM);
 			}
 		}
 
-		void HandleSearchEvent(SearchEvent<LMProject> searchEvent)
+		void HandleSearchEvent (SearchEvent<LMProject> searchEvent)
 		{
-			HandleSearch(searchEvent.TextFilter);
-		}
-
-		void HandleSearch(string text)
-		{
-			foreach (var lmProjectVM in ViewModel.ViewModels)
-			{
-				lmProjectVM.Visible = lmProjectVM.Model.Description.Search(text);
+			foreach (var lmProjectVM in ViewModel.ViewModels) {
+				lmProjectVM.Visible = lmProjectVM.Model.Description.Search (searchEvent.TextFilter);
 			}
 			ViewModel.VisibleViewModels.ApplyPropertyChanges ();
 			ViewModel.NoResults = !ViewModel.VisibleViewModels.Any ();
