b9a10caa5bc297b4c35a8e58207cec3b10dc37d3
Andoni Morales Alastruey <ylatuya@gmail.com>
2017-12-11 11:10:12 +0100

    Add resources classes

---
+++ LongoMatch.Core/Resources/Images.cs
@@ -0,0 +1,16 @@
+﻿//
+//  Copyright (C) 2017 Fluendo S.A.
+//
+//
+using System;
+using VAS.Core.Common;
+
+namespace LongoMatch.Core.Resources
+{
+	public class Images : VAS.Core.Resources.Images
+	{
+		public const string EmptyArrow = "images/lm-empty-arrow" + Constants.IMAGE_EXT;
+		public const string AnyLogoGreen = "images/lm-any-logo-green" + Constants.IMAGE_EXT;
+		public const string MenuTopBackground = "images/lma-menu-top-bg" + Constants.IMAGE_EXT;
+	}
+}
---
+++ LongoMatch.Core/Resources/Strings.cs
@@ -0,0 +1,11 @@
+﻿//
+//  Copyright (C) 2017 Fluendo S.A.
+//
+//
+using System;
+namespace LongoMatch.Core.Resources
+{
+	public class Strings : VAS.Core.Resources.Strings
+	{
+	}
+}
